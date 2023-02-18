e798b8dda2932dda091267df968f8e312a5fc8e8
Gabriela <gabisantosdeveloper@gmail.com>
2023-02-18 16:52:44 -0300

    Fix some bugs

--- android_course_basic_layouts/coffe_app/app/src/main/java/com/gabriela/coffe/MainActivity.kt
+++ android_course_basic_layouts/coffe_app/app/src/main/java/com/gabriela/coffe/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.gabriela.coffe
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.gabriela.coffe.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , View.OnClickListener {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        binding.createAccountButton.setOnClickListener(this)
+        binding.enterButton.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        when(view.id) {
+            R.id.create_account_button -> openCreateAccountScreen()
+            R.id.enter_button -> openLoginScreen()
+        }
+    }
+
+    private fun openCreateAccountScreen() {
+        TODO("Not yet implemented")
+    }
+
+    private fun openLoginScreen() {
+        TODO("Not yet implemented")
     }
 }
