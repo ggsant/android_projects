3aa3c27c51701f87f5486104f8ecbff68923a583
Gabriela <gabisantosdeveloper@gmail.com>
2023-05-04 07:31:20 -0300

    feat: Create and refactor project

---
+++ android_course_basic_layouts/convidados/app/src/main/java/com/gabriela/convidados/ui/AbsentGuestFragment.kt
@@ -0,0 +1,42 @@
+package com.gabriela.convidados.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.gabriela.convidados.databinding.FragmentAbsentGuestBinding
+
+class AbsentGuestFragment : Fragment() {
+
+    private var _binding: FragmentAbsentGuestBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val slideshowViewModel =
+            ViewModelProvider(this).get(AbsentGuestViewModel::class.java)
+
+        _binding = FragmentAbsentGuestBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textSlideshow
+        slideshowViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ android_course_basic_layouts/convidados/app/src/main/java/com/gabriela/convidados/ui/PresentGuestFragment.kt
@@ -0,0 +1,42 @@
+package com.gabriela.convidados.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.gabriela.convidados.databinding.FragmentPresentGuestBinding
+
+class PresentGuestFragment : Fragment() {
+
+    private var _binding: FragmentPresentGuestBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val viewModel =
+            ViewModelProvider(this).get(PresentGuestViewModel::class.java)
+
+        _binding = FragmentPresentGuestBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textGallery
+        viewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
